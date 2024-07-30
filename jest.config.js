81cafbe610d70e7888ed401792a32fd63e45c721
Felipe Cesar <felipecesr@gmail.com>
2024-07-30 11:07:24 -0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,5 @@ export default {
   moduleNameMapper: {
     "^~/(.+)": "<rootDir>/src/$1",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
