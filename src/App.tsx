e35b570a048df640c3baa51a078880e75ed19510
Felipe Cesar <felipecesr@gmail.com>
2024-08-04 09:41:45 -0300

    chore: use confirm dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Dialog } from "~/components/Dialog";
+import { ConfirmDialog } from "~/components/ConfirmDialog";
 import Router from "~/router";
 import { Header } from "./components/Header";
 import { useRegistrations } from "./store/registration";
@@ -13,7 +13,7 @@ function App() {
         <h1>Caju Front Teste</h1>
       </Header>
       <Router />
-      <Dialog />
+      <ConfirmDialog />
       <FeedbackDialog
         isOpen={state.status === "success" || state.status === "failure"}
         type={state.status}
